47ed650f461d1092896f74d26c7e52a02d57d036
bergurisak <ibergur@gmail.com>
2025-03-24 17:48:29 +0000

    Controllers og fxml skrár

--- src/main/java/com/example/safecasino/RoulletteController.java
+++ src/main/java/com/example/safecasino/RoulletteController.java
@@ -1,2 +1,4 @@
+package com.example.safecasino;
+
 public class RoulletteController {
 }
--- src/main/java/com/example/safecasino/SafeCasinoApplication.java
+++ src/main/java/com/example/safecasino/SafeCasinoApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class SafeCasinoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SafeCasinoApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SafeCasinoApplication.class.getResource("SafeCasino.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
