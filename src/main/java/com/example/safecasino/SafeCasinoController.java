829b8dc06ae90ad95f9cb9677e61caa3514a0dee
bergurisak <ibergur@gmail.com>
2025-03-24 17:45:14 +0000

    Controllers

--- src/main/java/com/example/safecasino/SafeCasinoController.java
+++ src/main/java/com/example/safecasino/SafeCasinoController.java
@@ -3,7 +3,7 @@ package com.example.safecasino;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class SafeCasinoController {
     @FXML
     private Label welcomeText;
 
