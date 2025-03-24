4f1a94789e5d7c8fb5f07325541a5db2f58e4974
olithgspam <olithjrspam@gmail.com>
2025-03-24 18:14:30 +0000

    ye

--- src/main/java/com/example/safecasino/HelloApplication.java
+++ src/main/java/com/example/safecasino/HelloApplication.java
@@ -19,6 +19,9 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+        System.out.println("Hello, World!");
     }
-    igga juice
-}
+
+
+
+
